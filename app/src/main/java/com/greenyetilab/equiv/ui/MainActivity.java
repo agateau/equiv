8d565e474fb808adddf06d4ad2df8595f0b158a9
Aurélien Gâteau <mail@agateau.com>
2015-09-19 23:51:21 +0200

    Add a "new day" item to the option menu

--- app/src/main/java/com/greenyetilab/equiv/ui/MainActivity.java
+++ app/src/main/java/com/greenyetilab/equiv/ui/MainActivity.java
@@ -1,6 +1,8 @@
 package com.greenyetilab.equiv.ui;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -94,17 +96,28 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        } else if (id == R.id.action_new_day) {
+            onNewDay();
+            return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    private void onNewDay() {
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.start_new_day)
+                .setPositiveButton(R.string.start_new_day_button, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mDay.clear();
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel, null)
+                .show();
     }
 
     private void updateTitle() {
