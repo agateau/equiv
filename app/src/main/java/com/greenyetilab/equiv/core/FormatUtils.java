008c05472fb5f7c07bfbf663dd6ac16551dbb7ee
Aurélien Gâteau <mail@agateau.com>
2015-09-28 21:56:29 +0200

    More precise naturalRound

--- app/src/main/java/com/greenyetilab/equiv/core/FormatUtils.java
+++ app/src/main/java/com/greenyetilab/equiv/core/FormatUtils.java
@@ -46,10 +46,15 @@ public class FormatUtils {
 
     public static String naturalRound(float value) {
         int rounded = Math.round(value);
-        if (Math.abs(value - rounded) < 0.2) {
+        if (Math.abs(value - rounded) < 0.01) {
             return String.valueOf(rounded);
         } else {
-            return String.format("%.1f", value);
+            String str = String.format("%.2f", value);
+            if (str.charAt(str.length() - 1) == '0') {
+                return str.substring(0, str.length() - 1);
+            } else {
+                return str;
+            }
         }
     }
 
