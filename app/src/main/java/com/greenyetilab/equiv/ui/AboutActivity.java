ca662dc942ba7d75e095aead1f9589a608da4fdb
Aurélien Gâteau <mail@agateau.com>
2015-10-06 19:11:01 +0200

    Nicer About text

--- app/src/main/java/com/greenyetilab/equiv/ui/AboutActivity.java
+++ app/src/main/java/com/greenyetilab/equiv/ui/AboutActivity.java
@@ -2,14 +2,14 @@ package com.greenyetilab.equiv.ui;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.method.LinkMovementMethod;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.greenyetilab.equiv.R;
+import com.greenyetilab.equiv.core.Constants;
+import com.greenyetilab.utils.NTemplate;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -22,7 +22,15 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        NTemplate template = new NTemplate(getText(R.string.about_description));
+        template.put("email", Constants.AUTHOR_EMAIL)
+                .put("rate_url", Constants.GPLAY_URL)
+                .put("gpl_url", Constants.GPL_URL)
+                .put("github_url", Constants.GITHUB_URL );
+
         TextView view = (TextView) findViewById(R.id.descriptionTextView);
+        view.setText(template.toSpanned());
         view.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
