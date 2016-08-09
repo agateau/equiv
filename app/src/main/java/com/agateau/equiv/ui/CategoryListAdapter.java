ac34f91964fd6f64fd50f7e56c513a89c375c4c9
Aurélien Gâteau <mail@agateau.com>
2016-08-09 08:17:41 +0200

    Show category icon in product category dropdown

--- app/src/main/java/com/agateau/equiv/ui/CategoryListAdapter.java
+++ app/src/main/java/com/agateau/equiv/ui/CategoryListAdapter.java
@@ -1,7 +1,12 @@
 package com.agateau.equiv.ui;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.agateau.equiv.R;
 import com.agateau.equiv.core.ProductCategory;
@@ -9,7 +14,45 @@ import com.agateau.equiv.core.ProductCategory;
 import java.util.List;
 
 public class CategoryListAdapter extends ArrayAdapter<ProductCategory> {
+    static class ViewHolder {
+        ImageView imageView;
+        TextView textView;
+    }
+
     public CategoryListAdapter(Context context, List<ProductCategory> categories) {
         super(context, R.layout.category_item, categories);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View view;
+        ViewHolder vh;
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            view = inflater.inflate(R.layout.category_item, parent, false);
+            vh = new ViewHolder();
+            vh.imageView = (ImageView) view.findViewById(R.id.product_category_image);
+            vh.textView = (TextView) view.findViewById(R.id.product_category_text);
+            view.setTag(vh);
+        } else {
+            view = convertView;
+            vh = (ViewHolder) view.getTag();
+        }
+
+        ProductCategory category = getItem(position);
+
+        // imageView
+        vh.imageView.setImageDrawable(ProductCategoryUtils.getDrawableForCategory(getContext(), category));
+
+        // textView
+        vh.textView.setText(category.toString());
+
+        return view;
+    }
+
+    @Override
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+        return getView(position, convertView, parent);
     }
 }
