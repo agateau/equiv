37ced6d691102f69738ce14eb566dd7939cac199
Aurélien Gâteau <mail@agateau.com>
2016-11-16 08:24:32 +0100

    Fix crash when editing a custom product
    Was crashing when using French locale if the product protein weight number
    was a rational.

--- app/src/main/java/com/agateau/equiv/core/FormatUtils.java
+++ app/src/main/java/com/agateau/equiv/core/FormatUtils.java
@@ -1,16 +1,22 @@
 package com.agateau.equiv.core;
 
+import java.util.Locale;
+
 /**
  * Text utilities
  */
 public class FormatUtils {
 
     public static String naturalRound(float value) {
+        return naturalRound(Locale.getDefault(), value);
+    }
+
+    public static String naturalRound(Locale locale, float value) {
         int rounded = Math.round(value);
         if (Math.abs(value - rounded) < 0.01) {
             return String.valueOf(rounded);
         } else {
-            String str = String.format("%.2f", value);
+            String str = String.format(locale, "%.2f", value);
             if (str.charAt(str.length() - 1) == '0') {
                 return str.substring(0, str.length() - 1);
             } else {
