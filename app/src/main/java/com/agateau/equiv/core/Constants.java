789bda739da3aed6511c1805827626e4f6658a7c
Aurélien Gâteau <mail@agateau.com>
2023-03-18 20:30:51 +0100

    http -> https

--- app/src/main/java/com/agateau/equiv/core/Constants.java
+++ app/src/main/java/com/agateau/equiv/core/Constants.java
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+    https://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -24,9 +24,9 @@ public class Constants {
     public static final float PROTEIN_FOR_POTATO = 0.02f;
 
     public static final String AUTHOR_EMAIL = "mail@agateau.com";
-    public static final String GPLAY_URL = "http://play.google.com/store/apps/details?id=com.agateau.equiv";
-    public static final String LICENSE_URL = "http://www.apache.org/licenses/LICENSE-2.0";
-    public static final String GITHUB_URL = "http://github.com/agateau/equiv";
+    public static final String GPLAY_URL = "https://play.google.com/store/apps/details?id=com.agateau.equiv";
+    public static final String LICENSE_URL = "https://www.apache.org/licenses/LICENSE-2.0";
+    public static final String GITHUB_URL = "https://github.com/agateau/equiv";
 
     public static final String CUSTOM_PRODUCTS_EMAIL = "equiv@agateau.com";
 }
